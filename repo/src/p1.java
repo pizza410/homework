156577a10d85902823360745d06b1ed66d2f2e74
pizza410 <shubhstudies98@gmail.com>
2024-04-07 20:12:32 +0100

    solution of problem1

---
+++ repo/src/p1.java
@@ -0,0 +1,12 @@
+public class p1 {
+    String s="abc";
+    int i=97;
+    public static void main(String[] args) {
+        p1 cd=new p1();
+        cd.call();
+    }
+    public void call(){
+        System.out.println(s);
+        System.out.println(i);
+    }
+}
