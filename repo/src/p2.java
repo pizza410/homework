5572336fca6d5fc36efa8c7a8d0c6a927ec44562
pizza410 <shubhstudies98@gmail.com>
2024-04-07 20:16:28 +0100

    solution of problem 2

---
+++ repo/src/p2.java
@@ -0,0 +1,11 @@
+public class p2 {
+    static String s="bvm";
+    static int i = 98;
+    public static void main(String[] args) {
+        call();
+
+    }
+    public static void call(){
+        System.out.println(s+" "+i);
+    }
+}
