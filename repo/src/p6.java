ecee8deb8d65a3bff95ddaeacd4886a4a9da0046
pizza410 <shubhstudies98@gmail.com>
2024-04-07 20:50:16 +0100

    solution of problem 6

--- repo/src/p6.java
+++ repo/src/p6.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class p6 {
+    public static void main(String[] args) {
+        System.out.print("Please enter a radius: ");
+        Scanner input = new Scanner(System.in);
+        float i=input.nextFloat();
+        area(i);
+    }
+    public static void area(Float i){
+        System.out.println("Area is "+(3.14*i*1));
+    }
+
 }
