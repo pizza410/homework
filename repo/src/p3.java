bbdbef3badc334c8ba80cbc1e521b44101fb4aa2
pizza410 <shubhstudies98@gmail.com>
2024-04-07 20:21:37 +0100

    solution of problem 3

---
+++ repo/src/p3.java
@@ -0,0 +1,16 @@
+public class p3 {
+    String s="kem cho Bhaveshbhai";
+    static int i=410;
+    public static void main(String[] args) {
+        call();
+        p3 dc=new p3();
+        dc.call2();
+    }
+    public static void call(){
+        p3 dc=new p3();
+        System.out.println(dc.s+" "+i);
+    }
+    public void call2(){
+        System.out.println(s+" "+i);
+    }
+}
