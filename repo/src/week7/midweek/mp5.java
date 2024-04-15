6188ced15871e53deeeea7d54862df90db85461a
pizza410 <shubhstudies98@gmail.com>
2024-04-15 20:46:00 +0100

    week 7 midweek homework

--- repo/src/week7/midweek/mp5.java
+++ repo/src/week7/midweek/mp5.java
@@ -1,4 +1,23 @@
 package week7.midweek;
 
+import java.util.Scanner;
+
 public class mp5 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Please enter first number: ");
+        double d1=input.nextDouble();
+        System.out.print("Please enter second number: ");
+        double d2=input.nextDouble();
+        greatest(d1,d2);
+    }
+
+    private static void greatest(double d1, double d2) {
+        if(d1>d2)
+            System.out.println(d1+" is bigger");
+        else if(d1==d2)
+            System.out.println("Both are same");
+        else
+            System.out.println(d2+" is bigger");
+    }
 }
